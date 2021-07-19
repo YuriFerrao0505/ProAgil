6cedd4b8a0879fa7d892d0f203664abd1f93a209
Yuri Ferrão <yuriferrao9@gmail.com>
2021-07-19 16:51:38 -0300

    Inicial do meu projeto de Eventos

---
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers
+{
+    [Route("site/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public ValuesController(DataContext context){
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           
+            var results = await _context.Eventos.ToListAsync();
+            return Ok(results);       
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var results = await _context.Eventos.FirstOrDefaultAsync(x=>x.EventoId == id);;
+            return Ok(results); 
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
